b3f6340556ae9b03f0e6d67face5a6d0dbe61550
Babolin <a64u@gft.com>
2019-05-29 16:38:27 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { RoomsComponent } from './accomodation/rooms/rooms.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      AccomodationComponent,
+      RoomsComponent,
+      FoodBeverageComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MaterialModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
