36e16f7fb5675fd7613c96bbd6f301eef0a4faeb
Babolin <a64u@gft.com>
2019-05-29 18:03:08 +0200

    add initial routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupComponent } from './auth/signup/signup.component';
+import { OffersComponent } from './offers/offers.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { SigninComponent } from './auth/signin/signin.component';
+
+const routes: Routes = [
+   { path: 'accomodation', component: AccomodationComponent },
+   { path: 'celebrations', component: CelebrationsComponent },
+   { path: 'food-and-beverage', component: FoodAndBeverageComponent },
+   { path: 'lifestyle', component: LifestyleComponent },
+   { path: 'offers', component: OffersComponent },
+   { path: 'weddings', component: WeddingsComponent },
+   { path: 'signin', component: SigninComponent },
+   { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+   imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+   exports: [RouterModule]
 })
 export class AppRoutingModule { }
